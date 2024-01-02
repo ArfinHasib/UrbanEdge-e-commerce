232be952d0aa8efc26603b71177a081d5e04f2c1
ArfinHasib <arfinhasib21375@gmail.com>
2024-01-02 11:42:22 +0600

    Top Products Section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar/Navbar';
 import Products from './components/Products/Products';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import TopProducts from './components/TopProducts/TopProducts';
 
 const App = () => {
    useEffect(() => {
@@ -22,6 +23,7 @@ const App = () => {
             <Navbar />
             <Hero />
             <Products />
+            <TopProducts />
          </div>
       </div>
    );
